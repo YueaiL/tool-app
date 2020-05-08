64adb571be04fcf2e2da44417b443d0bb819a4a2
zyz <1070947245@qq.com>
2020-05-08 18:17:49 +0800

    first

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Test
+    <button>{{msg}}</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "IndexPage",
+    data() {
+      return {
+        msg: '首页'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
